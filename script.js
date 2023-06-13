5fe513d770d27a1de5e2b92cd0b33216811bf164
ocatarama <catarama.ovidiu66@gmail.com>
2023-06-13 14:36:17 +0300

    add disabled attribute to button

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ boxes.forEach((element, index) => {
             points.innerHTML = parseInt(points.innerHTML) - 5;
 
             //Remove the box
-            $(element).css("backgroundColor", "transparent"); 
+            $(element).css("backgroundColor", "transparent");
+            $(element).attr("disabled", "disabled");
             $(element).css("boxShadow","none")
         }
     });
@@ -42,15 +43,17 @@ boxes.forEach((element, index) => {
 
 function GenerateColors() {
     /* Generating a random number to pick the color to be guessed */
-    solution = Math.round(Math.random() * 6);
-    while(!solution)
-        solution = Math.round(Math.random() * 6);
+    solution = Math.round(Math.random() * 5) + 1;
+
+    // while(!solution)
+    //     solution = Math.round(Math.random() * 6);
 
     for(var i = 0; i < 6; ++i)
     {
         //Remake all the boxes
         $(boxes[i]).css("display", "block"); 
         $(boxes[i]).css("boxShadow", "4px 4px 10px rgba(0,0,0,.2)");
+        $(boxes[i]).removeAttr("disabled");
 
         //Generate color
         const redT = Math.round(Math.random() * 255);
